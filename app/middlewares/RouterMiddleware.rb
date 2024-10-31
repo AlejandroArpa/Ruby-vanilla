08cf379b474bb5ed3cdc728aef574c5390f1709b
Alejandro Ramirez P <alejorp195@hotmail.com>
2024-10-31 10:21:13 -0500

    pathparams done

--- app/middlewares/RouterMiddleware.rb
+++ app/middlewares/RouterMiddleware.rb
@@ -10,8 +10,11 @@ class RouterMiddleware
   def call(env)
     req = Rack::Request.new(env)
     route = Router.find_route(req.path_info, req.request_method)
-    puts route
     if route
+      if route[:param]
+        param = req.path_info.split('/')[2]
+        env['path_param'] = param
+      end
       # Ruta encontrada, procesar el controlador y la acción
       controller_name, action_name = route[:to].split('#')
       env['controller_name'] = controller_name
--- app/router/routes.rb
+++ app/router/routes.rb
@@ -1,6 +1,8 @@
 
 Router.draw do
-  get "/reports", to: "reports#getAll"
-  post "/reports", to: "reports#create"
-  delete "/reports/:id", to: "reports#delete"
+  get "reports", false, to: "reports#getAll" 
+  get "reports", true, to: "reports#getOne"
+  post "reports", to: "reports#create"
+  delete "reports/:id", true, to: "reports#delete"
+  put  "reports", to: "reports#edit"
 end
